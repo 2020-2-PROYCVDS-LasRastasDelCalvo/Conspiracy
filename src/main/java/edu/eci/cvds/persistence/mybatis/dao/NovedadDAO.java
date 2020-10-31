4f1482c350ac95a69e3a29b8e933be62b071a54c
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2020-10-31 18:45:38 -0500

    MyBatisDAO listo

--- src/main/java/edu/eci/cvds/persistence/mybatis/dao/NovedadDAO.java
+++ src/main/java/edu/eci/cvds/persistence/mybatis/dao/NovedadDAO.java
@@ -1,8 +1,9 @@
-package edu.eci.cvds.persistence.mybatis.mappers;
+package edu.eci.cvds.persistence.mybatis.dao;
 
 import edu.eci.cvds.entities.Novedad;
 import edu.eci.cvds.persistence.PersistenceException;
 import org.apache.ibatis.annotations.Param;
+
 import java.util.List;
 
 /**
@@ -12,20 +13,20 @@ import java.util.List;
  * @author Iván Camilo Rincón Saavedra
  * @version 10/31/2020
  */
-public interface NovedadMapper {
+public interface NovedadDAO  {
 
     /**
      * Metodo que consulta todas la novedades que se encuentran en la base de datos ordenada de la mas reciente a las mas antigua.
      * @return List<Novedad>, lista de novedades.
      */
-    public List<Novedad> consultarNovedades() throws PersistenceException;
+    public List<Novedad> cargarNovedades() throws PersistenceException;
 
     /**
      * Metodo que consulta el historial de novedades que ha tenido un equipo especifico
      * @param idEquipo, int que representa el identificador del equipo
      * @return  List<Novedad>, la lista de novedades que posee ese equipo
      */
-    public List<Novedad> consultarNovedadEquipo( @Param("idEquipo") int idEquipo )  throws PersistenceException;
+    public List<Novedad> cargarNovedadesEquipo( @Param("idEquipo") int idEquipo )  throws PersistenceException;
 
 
     /**
@@ -33,14 +34,14 @@ public interface NovedadMapper {
      * @param idElemento, int que representa el identificador del elemento
      * @return  List<Novedad>, la lista de novedades que posee ese equipo
      */
-    public List<Novedad> consultarNovedadElemento( @Param("idElemento") int idElemento ) throws PersistenceException;
+    public List<Novedad> cargarNovedadesElemento( @Param("idElemento") int idElemento ) throws PersistenceException;
 
     /**
      * Metodo que se encarga de añadir una nueva novedad para un equipo en la base de datos
      * @param novedad, Novedad novedad a insertar en la base de datos
      * @param idEquipo, int id del equipo al cual se le insertara la novedad
      */
-    public void addNovedadEquipo( @Param("novedad") Novedad novedad, @Param("idEquipo") int idEquipo ) throws PersistenceException;
+    public void guardarNovedadEquipo( @Param("novedad") Novedad novedad, @Param("idEquipo") int idEquipo ) throws PersistenceException;
 
     /**
      * Metodo que se encarga de añadir una nueva novedad para un elemento en la base de datos
@@ -48,5 +49,5 @@ public interface NovedadMapper {
      * @param idEquipo, int id del equipo al cual se le insertara la novedad
      * @param idElemento, , int id del elemento al cual se le insertara la novedad
      */
-    public void addNovedadElemento( @Param("novedad") Novedad novedad, @Param("idEquipo") int idEquipo, @Param("idElemento") int idElemento ) throws PersistenceException;
+    public void guardarNovedadElemento( @Param("novedad") Novedad novedad, @Param("idEquipo") int idEquipo, @Param("idElemento") int idElemento ) throws PersistenceException;
 }
